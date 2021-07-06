ec9d040f8c2294d7f97cb86a9d2d1689f06254a8
DuongNguyenTrung <duongtongmon3@gmail.com>
2021-07-07 01:25:46 +0700

    fghjk

--- src/informationofpatient/Inputter.java
+++ src/informationofpatient/Inputter.java
@@ -6,35 +6,17 @@ import java.util.Scanner;
 
 public class Inputter {
     public static Scanner sc = new Scanner(System.in);
-    public static String Inputername(String msg){
-        System.out.println(msg);
-        String name = sc.nextLine();
-        return name;
-    }
-    public static int InputerAge(String msg){
+ 
+    public static int InputerInt(String msg){
         System.out.println(msg);
         int age = Integer.parseInt(sc.nextLine());
         return age;
     }
-    public static String InputerSex(String msg){
+    public static String InputerString(String msg){
         System.out.println(msg);
         String sex = sc.nextLine();
         return sex;
     }
-    public static String InputerAddress(String msg){
-        System.out.println(msg);
-        String Address = sc.nextLine();
-        return Address;
-    }
-    public static String InputerPhone(String msg){
-        System.out.println(msg);
-        String Phone = sc.nextLine();
-        return Phone;
-    }
-    public static String InputerSotheBHYT(String msg){
-        System.out.println(msg);
-        String sothe = sc.nextLine();
-        return sothe;
-    }
+   
     
 }
